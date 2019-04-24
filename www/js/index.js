7838b1246d2a2453b5a3c96334acb9e50cdf3f88
laerke-s <laerkes@stud.ntnu.no>
2019-04-24 16:07:12 +0200

    Refactored to Ratchet
    Changed implementation to work with Ratchet instead of the tutorial
    solution. Now use Push.js to change pages.

--- www/js/index.js
+++ www/js/index.js
@@ -2,23 +2,47 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
-    StartMenuView.prototype.template = Handlebars.compile($("#home-tpl").html());
-    MapView.prototype.template = Handlebars.compile($("#map-tpl").html());
-    var slider = new PageSlider($('body'));
-    router.addRoute('', function () {
-        slider.slidePage(new StartMenuView().render().$el);
-    });
-    router.addRoute('map', function () {
-        var mapPage = new MapView();
-        slider.slidePage(mapPage.render().$el);
-        /*Must do it like this to ensure that mapcontainer has been made*/
-        mapPage.initMap();
-    });
-    router.start();
+    var map;
+
     /* --------------------------------- Event Registration -------------------------------- */
     document.addEventListener('deviceready', function () {
         FastClick.attach(document.body);
     }, false);
+    window.addEventListener('push', checkPage);
     /* ---------------------------------- Local Functions ---------------------------------- */
+    function checkPage() {
+        if (document.getElementById('map-container')) {
+            loadMap('map-container');
+        }
+    };
+
+    function loadMap(id) {
+        map = L.map(id).fitWorld();
+        L.tileLayer('https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo4&zoom={z}&x={x}&y={y}', {
+            attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
+        }).addTo(map);
+        L.control.scale({
+            imperial: false
+        }).addTo(map);
+        map.locate({
+            setView: true,
+            maxZoom: 16,
+            maximumAge: 3600000,
+            enableHighAccuracy: true,
+        });
+        map.on('locationfound', onLocationFound);
+        map.on('locationerror', onLocationError);
+    };
+
+    onLocationFound = function (e) {
+        var radius = e.accuracy / 2;
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("You have found yourself at<br/>" + e.latlng).openPopup();
+        L.circle(e.latlng, radius).addTo(map);
+    };
+
+    onLocationError = function (e) {
+        alert(e.message);
+    };
 
 }());
